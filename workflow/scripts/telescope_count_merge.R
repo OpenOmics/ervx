a8ce060e72e7c3fda0f0221cc9a86e472ef48cfb
asyakhl <asyakhl@users.noreply.github.com>
2023-03-21 11:11:00 -0400

    fix for telescope_count_merge.R script for merge_telescope rule

--- workflow/scripts/telescope_count_merge.R
+++ workflow/scripts/telescope_count_merge.R
@@ -1,5 +1,6 @@
 args <- commandArgs(trailingOnly = TRUE)
-files <- as.character(args[1])
+files <- args[1:length(args)]
+print("FILES:")
 print(files)
 l <- list()
 setwd(getwd())
@@ -8,9 +9,15 @@ for(i in seq_along(files)){
   colnames(f)[2]=unlist(strsplit(tail(unlist(strsplit(files[i], "/")),n=1),"-"))[1]
   l[[i]]=f
 }
+print("LIST:")
 print(l)
+print(length(l))
 counts_merged <- Reduce(function (...) { merge(..., all = FALSE, by = "transcript") },   # Inner join
           l)
-print(counts_merged)
+print("COUNTS_MERGED:")
+print(head(counts_merged))
+
+print("DIMS counts_merged:")
+print(dim(counts_merged))
 write.csv(counts_merged, "telescope/counts_merged.csv")
 
