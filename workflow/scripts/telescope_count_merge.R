1d71e1274f17c6f8075bceeaae16cdda6e249609
asyakhl <asyakhl@users.noreply.github.com>
2023-04-04 15:24:32 -0400

    added heatmap for families

--- workflow/scripts/telescope_count_merge.R
+++ workflow/scripts/telescope_count_merge.R
@@ -1,4 +1,6 @@
 library(dplyr)
+library(reshape)
+library(ggplot2)
 args <- commandArgs(trailingOnly = TRUE)
 
 family_anno_table <- args[length(args)]
@@ -8,10 +10,12 @@ family_anno_table <- read.csv(file=family_anno_table, header=T)
 files_length <- length(args)-1
 files <- args[1:files_length]
 
+setwd(getwd())
+
 print("FILES:")
 print(files)
 l <- list()
-setwd(getwd())
+
 for(i in seq_along(files)){
   f=read.table(files[i], header=T)
   colnames(f)[2]=unlist(strsplit(tail(unlist(strsplit(files[i], "/")),n=1),"-"))[1]
@@ -32,6 +36,7 @@ print("DIMS counts_merged:")
 print(dim(counts_merged))
 write.csv(counts_merged, "telescope/counts.csv", row.names=FALSE)
 
+# Counts Summing By Family Per Sample
 for(i in seq_along(counts_merged$transcript)){
   if(is.na(counts_merged$Repbase[i])){
     counts_merged$Family[i]="__no_feature"
@@ -52,3 +57,15 @@ counts_summed_by_family <- counts_merged %>%
 counts_summed_by_family=as.data.frame(counts_summed_by_family)
 write.csv(counts_summed_by_family,"telescope/counts_summed_by_family.csv", row.names=FALSE)
 
+# Heatmap: Counts Summed By Family Per Sample
+counts_summed_by_family$Family[which(is.na(counts_summed_by_family$Family))] <- "Unknown"
+transposed <- melt(counts_summed_by_family)
+ggplot(transposed, aes(x = variable, y = Family, fill = value)) +
+  geom_tile()+theme(axis.text.x = element_text(angle = 90,size=4,face="bold"),
+                    axis.text.y = element_text(size=4,face="bold"),
+                    plot.title = element_text())+
+  xlab("Samples")+ylab("Family")+ 
+  ggtitle("Counts Summed By Family Per Sample")
+  
+ggsave("telescope/counts_summed_by_family_heatmap.jpeg",
+       dpi=400, height=6, width=7, units="in")
